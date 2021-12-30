6bee531f8d49015514b3ea8e20a2c60d72ab2010
NoahTParkes <92675248+NoahTParkes@users.noreply.github.com>
2021-12-30 16:22:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Slutuppgift_Testning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
